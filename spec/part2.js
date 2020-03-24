a827871d307629b45cf91cc041d3347165f9c0ae
Matt McGavern <matt.mcgavern@gmail.com>
2020-03-24 18:58:10 -0500

    Fixed tests for _.extend

--- spec/part2.js
+++ spec/part2.js
@@ -191,7 +191,11 @@
       it('should override properties found on the destination', function() {
         // Replace this line with an `expect` statement that tests
         // the behavior described by the `it` string
-        throw new Error('This test is missing.');
+        var destination = { a: 2 };
+        var source = { a: 3, b: 4 };
+        var extended = _.extend(destination, source);
+
+        expect(extended.a).to.equal(3);
       });
 
       it('should not override properties not found in the source', function() {
